15cd45d341561bb8dcd1322d4493c5929ac248c5
Rex Burkybile <Rex Burkybile@DESKTOP-FLQ9MAC>
2019-10-25 03:37:41 -0500

    For English

---
+++ src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Rex Burkybile
+ */
+class Demo
+{
+     public Demo()
+     {
+         System.out.println("This is a no argument constructor");
+     }
+     public static void main(String args[]) {
+    	 new Demo();
+     }
+}
